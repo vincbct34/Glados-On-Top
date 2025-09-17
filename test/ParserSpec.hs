50f8b9c158eb86778a6a7abc3f3fdddf5467948f
Samuel Bleau <samuel.bleau@epitech.eu>
2025-09-17 19:04:30 +0200

    [ADD]: Enhance error handling tests and add edge case scenarios in ParserSpec

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -153,3 +153,112 @@ spec = do
       case chainErrors [GenericError "a", GenericError "b"] of
         GenericError msg -> msg `shouldContain` "Multiple errors"
         _ -> expectationFailure "Expected GenericError"
+
+    it "returns unknown error on empty list" $ do
+      chainErrors [] `shouldBe` GenericError "Unknown error"
+
+    it "combines errors of different types" $ do
+      let e1 = GenericError "foo"
+          e2 = PError (UnexpectedChar 'x' Nothing)
+      case chainErrors [e1, e2] of
+        GenericError msg -> do
+          msg `shouldContain` "foo"
+          msg `shouldContain` "unexpected character"
+        _ -> expectationFailure "Expected GenericError"
+
+  ---------------------------------------------------------------------------
+  -- Show instances for errors
+  ---------------------------------------------------------------------------
+  describe "Show instances for errors" $ do
+    it "shows ParseError with location" $ do
+      let err = PError (UnexpectedChar 'x' (Just (SourceLocation 2 3 5)))
+      show err `shouldContain` "line 2, column 3"
+
+    it "shows SemanticError with location" $ do
+      let err = SError (UndefinedVariable "foo" (Just (SourceLocation 1 2 3)))
+      show err `shouldContain` "undefined variable 'foo'"
+
+    it "shows RuntimeError with location" $ do
+      let err = RError (RuntimeError "fail" (Just (SourceLocation 4 5 6)))
+      show err `shouldContain` "Runtime error at line 4, column 5"
+
+    it "shows GenericError" $ do
+      let err = GenericError "msg"
+      show err `shouldContain` "msg"
+
+  ---------------------------------------------------------------------------
+  -- Cas limites et erreurs supplémentaires
+  ---------------------------------------------------------------------------
+  describe "parseCharCase (edge cases)" $ do
+    it "fails on empty input" $ do
+      runParser (parseCharCase 'a') "" `shouldBe` Left (PError (UnexpectedEOF Nothing))
+
+  describe "parseAnyCharCase (edge cases)" $ do
+    it "fails on empty input" $ do
+      runParser (parseAnyCharCase "abc") "" `shouldBe` Left (PError (UnexpectedEOF Nothing))
+
+  describe "parseAnd (failures)" $ do
+    it "fails if first parser fails" $ do
+      runParser (parseAnd (parseChar 'a') (parseChar 'b')) "xbc"
+        `shouldBe` Left (PError (ExpectedChar 'a' Nothing))
+    it "fails if second parser fails" $ do
+      runParser (parseAnd (parseChar 'a') (parseChar 'b')) "axc"
+        `shouldBe` Left (PError (ExpectedChar 'b' Nothing))
+
+  describe "parseAndWith (failures)" $ do
+    it "fails if second parser fails" $ do
+      runParser (parseAndWith (\a b -> [a, b]) (parseChar 'a') (parseChar 'b')) "axc"
+        `shouldBe` Left (PError (ExpectedChar 'b' Nothing))
+
+  describe "parseMany (edge)" $ do
+    it "parses until parser fails" $ do
+      runParser (parseMany (parseChar 'a')) "aaabbb"
+        `shouldBe` Right ("aaa", "bbb")
+
+  describe "parseSome (single occurrence)" $ do
+    it "parses single occurrence" $ do
+      runParser (parseSome (parseChar 'a')) "abbb"
+        `shouldBe` Right ("a", "bbb")
+
+  describe "parseUInt (failures)" $ do
+    it "fails on empty input" $ do
+      runParser parseUInt "" `shouldBe` Left (GenericError "Expected unsigned integer")
+    it "fails on special chars" $ do
+      runParser parseUInt "!@#" `shouldBe` Left (GenericError "Expected unsigned integer")
+
+  describe "parseInt (failures)" $ do
+    it "fails on only sign" $ do
+      runParser parseInt "+" `shouldBe` Left (GenericError "Expected unsigned integer")
+      runParser parseInt "-" `shouldBe` Left (GenericError "Expected unsigned integer")
+    it "fails on sign with non-digit" $ do
+      runParser parseInt "+abc" `shouldBe` Left (GenericError "Expected unsigned integer")
+      runParser parseInt "-abc" `shouldBe` Left (GenericError "Expected unsigned integer")
+
+  describe "parseRight/parseLeft (failures)" $ do
+    it "fails if left parser fails (parseRight)" $ do
+      runParser (parseRight (parseChar 'a') (parseChar 'b')) "xbc"
+        `shouldBe` Left (PError (ExpectedChar 'a' Nothing))
+    it "fails if right parser fails (parseRight)" $ do
+      runParser (parseRight (parseChar 'a') (parseChar 'b')) "axc"
+        `shouldBe` Left (PError (ExpectedChar 'b' Nothing))
+    it "fails if left parser fails (parseLeft)" $ do
+      runParser (parseLeft (parseChar 'a') (parseChar 'b')) "xbc"
+        `shouldBe` Left (PError (ExpectedChar 'a' Nothing))
+    it "fails if right parser fails (parseLeft)" $ do
+      runParser (parseLeft (parseChar 'a') (parseChar 'b')) "axc"
+        `shouldBe` Left (PError (ExpectedChar 'b' Nothing))
+
+  describe "withError (failures)" $ do
+    it "overrides any error" $ do
+      runParser (withError (parseChar 'a') (GenericError "fail")) "" `shouldBe` Left (GenericError "fail")
+
+  describe "withContext (all cases)" $ do
+    it "adds context to RError" $ do
+      withContext "CTX" (RError (RuntimeError "fail" Nothing))
+        `shouldBe` RError (RuntimeError "CTX: fail" Nothing)
+    it "adds context to PError InvalidSyntax" $ do
+      withContext "CTX" (PError (InvalidSyntax "fail" Nothing))
+        `shouldBe` PError (InvalidSyntax "CTX: fail" Nothing)
+    it "does not change other errors" $ do
+      withContext "CTX" (PError (UnexpectedChar 'x' Nothing))
+        `shouldBe` PError (UnexpectedChar 'x' Nothing)
